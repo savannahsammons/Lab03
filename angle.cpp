59af20fc2da561e374d991fdd38262a0ffb50dfc
savannahsammons <sam20003@byui.edu>
2025-01-22 13:17:58 -0700

    Made stubbs for methods in angle class

--- angle.cpp
+++ angle.cpp
@@ -1,24 +1,162 @@
 /***********************************************************************
- * Source File:
- *    ANGLE
- * Author:
- *    Br. Helfrich
- * Summary:
- *    Everything we need to know about a direction
- ************************************************************************/
+* Source File:
+*    ANGLE
+* Author:
+*    Br. Helfrich
+* Summary:
+*    Everything we need to know about a direction
+************************************************************************/
 
 #include "angle.h"
 #include <math.h>  // for floor()
 #include <cassert>
 using namespace std;
 
- /************************************
-  * ANGLE : NORMALIZE
-  ************************************/
+
+/************************
+* Angle : ......................
+*******************/
+class Angle
+{
+private:
+   double radian;
+
+public:
+   /************************************
+   * Angle : constructor for angle class
+   ************************************/
+   Angle()
+   {
+      // constructor
+   }
+
+   /************************************
+   * Angle : .............
+   ************************************/
+   Angle(double degrees)
+   {
+      // something
+   }
+
+   /************************************
+   * Angle : .......
+   ************************************/
+   Angle(Angle rhs)
+   {
+      // something
+   }
+
+   /************************************
+   * getDegrees : .......
+   ************************************/
+   double getDegrees()
+   {
+      return 0.0; // something...
+   }
+
+   /************************************
+   * getRadians : .......
+   ************************************/
+   double getRadians()
+   {
+      return 0.0; // something...
+   }
+
+   /************************************
+   * setDegrees : .......
+   ************************************/
+   void setDegrees(double degrees)
+   {
+      // something...
+   }
+
+   /************************************
+   * setRadians : .......
+   ************************************/
+   void setRadians(double radians)
+   {
+      // something...
+   }
+
+   /************************************
+   * setRadians : .......
+   ************************************/
+   void setUp()
+   {
+      // something...
+   }
+
+   /************************************
+   * setRadians : .......
+   ************************************/
+   void setDown()
+   {
+      // something...
+   }
+
+   /************************************
+   * setRadians : .......
+   ************************************/
+   void setLeft()
+   {
+      // something...
+   }
+
+   /************************************
+   * setRadians : .......
+   ************************************/
+   void setRight()
+   {
+      // something...
+   }
+
+   /************************************
+   * setRadians : .......
+   ************************************/
+   void reverse()
+   {
+      // something...
+   }
+
+   /************************************
+   * setRadians : .......
+   ************************************/
+   void addOnto(double delta)
+   {
+      // something...
+   }
+
+private:
+   /************************************
+   * setRadians : .......
+   ************************************/
+   double convertToDegrees(double radians)
+   {
+      return 0.0;
+   }
+   /************************************
+   * setRadians : .......
+   ************************************/
+   double convertToRadians(double degrees)
+   {
+      return 0.0;
+   }
+
+   /************************************
+   * setRadians : .......
+   ************************************/
+   void normalize(double radians)
+   {
+      // something.......
+   }
+};
+
+
+/************************************
+* ANGLE : NORMALIZE
+************************************/
 double Angle::normalize(double radians) const
 {
    return -99.9;
 }
-
-
 
